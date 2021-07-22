6abc8a99a400e784ca3231871b7e7da9515479ed
vadyanator <vadok.hohol1999@gmail.com>
2021-07-22 20:34:17 +0300

    Updates

---
+++ precache-manifest.c731f70f6c0b36a06a6852c38b23d0a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "44808102085ce802d283b9dddae53074",
+    "url": "/start-project/static/media/preloader.44808102.gif"
+  },
+  {
+    "revision": "717ad4217ff236566883",
+    "url": "/start-project/static/js/runtime~main.717ad421.js"
+  },
+  {
+    "revision": "57c653e4e1bbb5ce1ec7",
+    "url": "/start-project/static/js/main.57c653e4.chunk.js"
+  },
+  {
+    "revision": "f94a0070b450aa56ff54",
+    "url": "/start-project/static/js/1.f94a0070.chunk.js"
+  },
+  {
+    "revision": "57c653e4e1bbb5ce1ec7",
+    "url": "/start-project/static/css/main.108814dc.chunk.css"
+  },
+  {
+    "revision": "8c62017007ac185273d0921d0e5356e9",
+    "url": "/start-project/index.html"
+  }
+];
